f5ec945fab07b84cf81eb7a8fd6a5c20ef5b8eec
ashumlp <ashumlp05@gmail.com>
2024-07-22 20:39:56 +0530

    first commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,11 @@
+import Banner from "./Component/Banner";
+import Navbar from "./Component/Navbar";
+
+export default function App() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    </>
+  )
+}
---
+++ FrontEnd/src/Component/Banner.jsx
@@ -0,0 +1,12 @@
+export default function Banner() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container max-auto md:px-20 px-4 flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2">
+        <h1 className="text-4xl">Hello, Welcomes Here to learn something new Everyday</h1>
+        </div>
+        <div className="w-full md:w-1/2">Right</div>
+      </div>
+    </>
+  );
+}
